180383ef07f038e35c0f3211a4be70a5bd8d5f0d
AgustinOlivaDS <agustinoliva@gmail.com>
2022-01-31 23:30:54 -0300

    Pug

--- Entregables/Templates/PugTemplate/index.js
+++ Entregables/Templates/PugTemplate/index.js
@@ -1,14 +1,42 @@
 const express = require('express')
+const pug = require('pug')
+
 const app = express()
 const PORT = 8080
-const routes = require('./routes/index')
+
+const Products = require('./src/products.js')
+const Prods = new Products()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/api', routes)
-
 app.use('/', express.static(__dirname + '/public'))
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.post('/productos', (req, res) => {
+    const producto = req.body
+    Prods.save(producto)
+    res.redirect('/')
+})
+
+app.get('/productos', (req, res) => {
+    const prods = Prods.getAll()
+    if(typeof prods.productos === 'undefined'){
+        console.log(`len del productos: undefined, error: ${prods.error}`)
+        res.render("vista", {
+            allProducts: prods,
+            hayProductos: false
+        });
+    } else { 
+        console.log(`len del productos: ${prods.productos.length}, error: ${prods.error}`)
+        res.render("vista", {
+            allProducts: prods,
+            hayProductos: (prods.productos.length > 0)
+        });
+    }
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server linstening on port ${server.address().port}`)
